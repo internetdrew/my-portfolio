3707c3c1474fea5e74e6527f02754ab81dc0f0ad
Andrew Rowley <andrew@internetdrew.com>
2022-09-15 14:56:56 -0400

    Add intersection observer for up arrow

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 'strict mode';
+
+const sections = document.querySelectorAll('section');
+const arrow = document.getElementById('up-arrow');
+const showArrow = function (e) {
+  if (e[0].isIntersecting) arrow.style.opacity = 1;
+  if (!e[0].isIntersecting) arrow.style.opacity = 0;
+};
+
+const observer = new IntersectionObserver(showArrow);
+
+observer.observe(sections[0]);
