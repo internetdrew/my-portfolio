df3518bcae1c340ef9bb2423b94f8813984b1930
Andrew Rowley <andrew@internetdrew.com>
2022-09-15 16:24:09 -0400

    Rebuild observer function

--- index.js
+++ index.js
@@ -2,11 +2,16 @@
 
 const sections = document.querySelectorAll('section');
 const arrow = document.getElementById('up-arrow');
-const showArrow = function (e) {
-  if (e[0].isIntersecting) arrow.style.opacity = 1;
-  if (!e[0].isIntersecting) arrow.style.opacity = 0;
+
+const toggleArrow = function () {
+  const showArrow = function (e) {
+    if (e[0].isIntersecting) arrow.style.opacity = 1;
+    if (!e[0].isIntersecting) arrow.style.opacity = 0;
+  };
+
+  const observer = new IntersectionObserver(showArrow);
+  observer.observe(sections[0]);
 };
 
-const observer = new IntersectionObserver(showArrow);
-
-observer.observe(sections[0]);
+// Event handlers
+window.addEventListener('scroll', toggleArrow);
