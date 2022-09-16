408a830953c8d5df9a15ed78942c1c12239b7cfd
Andrew Rowley <andrew@internetdrew.com>
2022-09-16 06:09:08 -0400

    Add functions for arrow showing and scrolling

--- index.js
+++ index.js
@@ -3,15 +3,22 @@
 const sections = document.querySelectorAll('section');
 const arrow = document.getElementById('up-arrow');
 
-const toggleArrow = function () {
-  const showArrow = function (e) {
-    if (e[0].isIntersecting) arrow.style.opacity = 1;
-    if (!e[0].isIntersecting) arrow.style.opacity = 0;
-  };
+const showArrow = function (entry) {
+  console.log(entry[0].isIntersecting);
+  if (entry[0].isIntersecting) arrow.style.opacity = 1;
+  if (!entry[0].isIntersecting) arrow.style.opacity = 0;
+};
 
-  const observer = new IntersectionObserver(showArrow);
+const toggleArrow = function () {
+  const observer = new IntersectionObserver(showArrow, { threshold: 1 });
   observer.observe(sections[0]);
+};
+
+const scrollToTop = function () {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 };
 
 // Event handlers
 window.addEventListener('scroll', toggleArrow);
+arrow.addEventListener('click', scrollToTop);
