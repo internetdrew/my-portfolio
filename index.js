9091ceade81c3516772d93f99f9c869b1e748717
Andrew Rowley <andrew@internetdrew.com>
2022-09-16 16:36:17 -0400

    Check contact form for required inputs

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 
 const header = document.querySelector('header');
 const arrow = document.getElementById('up-arrow');
+const form = document.getElementById('form');
+const nameEl = document.getElementById('name');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
 
 const showArrow = function (entry) {
-  console.log(entry[0].isIntersecting);
   if (!entry[0].isIntersecting) arrow.style.opacity = 1;
   if (entry[0].isIntersecting) arrow.style.opacity = 0;
 };
@@ -22,6 +25,37 @@ const scrollToTop = function () {
   document.documentElement.scrollTop = 0;
 };
 
+const showError = function (input, msg) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector('small');
+
+  formControl.classList.add('error');
+  small.textContent = msg;
+};
+
+const showsuccess = function (input) {};
+
+const checkRequired = function (inputArr) {
+  inputArr.forEach(input => {
+    if (!input.value.trim()) {
+      showError(input, `${input.id} is required`);
+      return;
+    }
+
+    if (input.value.trim()) {
+      showSuccess(input);
+    }
+  });
+};
+
 // Event handlers
 window.addEventListener('scroll', toggleArrow);
 arrow.addEventListener('click', scrollToTop);
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  checkRequired([nameEl, email, message]);
+
+  return;
+  form.submit();
+});
